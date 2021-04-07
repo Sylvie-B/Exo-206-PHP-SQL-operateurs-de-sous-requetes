3fb3839ee00569acc0fee99a66988271125411e8
Sylvie-B <unsolo@hotmail.fr>
2021-04-07 15:40:23 +0200

    connexion $search username, titre

--- index.php
+++ index.php
@@ -10,3 +10,41 @@
  *
  * ( PS: Sélectionnez, mais affichez le résultat à chaque fois ! ).
  */
+
+$server = 'localhost';
+$database = 'exo206';
+$user = 'root';
+$pass = '';
+
+try {
+    $conn = new PDO("mysql:host=$server;dbname=$database;charset=utf8", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $search = $conn->prepare("SELECT username FROM user");
+    $search->execute();
+
+    echo "<pre>";
+    print_r($search->fetchAll());
+    echo "</pre>";
+
+    $search = $conn->prepare("SELECT titre FROM article");
+    $search->execute();
+
+    echo "<pre>";
+    print_r($search->fetchAll());
+    echo "</pre>";
+
+    $search = $conn->prepare("SELECT * FROM article WHERE titre LIKE ('%poterie%')");
+    $search->execute();
+
+    echo "<pre>";
+    print_r($search->fetchAll());
+    echo "</pre>";
+
+
+} catch (PDOException $exception) {
+    echo "data base connexion error : " . $exception->getMessage();
+    return null;
+}
+
